24461c804e1c40f8ff71de1d714e4cdd57ff41f3
HABIBKHANPY <157411771+HABIBKHANPY@users.noreply.github.com>
2024-02-27 00:57:44 +0500

    Update magic_8_ball.py

--- magic_8_ball.py
+++ magic_8_ball.py
@@ -24,29 +24,7 @@ def magic_8_ball_game():
 
     # Magic 8 Ball answers
     responses = [
-       M
-import streamlit as st
-import random
-st.balloons()
-st.snow()
-
-# Sidebar menu
-st.sidebar.title("Menu")
-selected_page = st.sidebar.radio("", ["MAGIC 8 BALL GAME", "About Us", "Contact"])
-
-if selected_page == "MAGIC 8 BALL GAME":
-    st.title("BANO QABIL 2.0 PROJECT")
-    st.title("MAGIC 8 BALL")
-        # Add address book functionality here
-elif selected_page == "About Us":
-    st.title("About Us")
-    st.write(f"This address book application was created by {name}.")
-elif selected_page == "Contact":
-    st.title("Contact")
-    st.write("For support, please email support@example.com.")
-# Magic 8 Ball answers
-responses = [
-    "It is certain.",
+         "It is certain.",
     "It is decidedly so.",
     "Without a doubt.",
     "Yes - definitely.",
@@ -68,19 +46,6 @@ responses = [
     "Very doubtful."
 ]
 
-def main():
-    # Custom CSS to inject into the webpage
-    st.markdown("""
-        <style>
-        .stApp {
-            background-image: url('https://img.hotimg.com/eight-ball.jpeg');
-            background-size: cover;
-            color: #ffffff;
-        }
-        .stTextInput>div>div>input {
-    color: #e0e0e0;
-    background-color: #333333;
-    }
     ]
 
     question = st.text_input("Ask a question:")
