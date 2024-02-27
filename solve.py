6248fcee8e045d3fd53c29d580ba4c35d6af7dcc
HABIBKHANPY <157411771+HABIBKHANPY@users.noreply.github.com>
2024-02-27 15:46:42 +0500

    Update solve.py

--- solve.py
+++ solve.py
@@ -26,7 +26,23 @@ def magic_8_ball_game():
     responses = [
         "It is certain.",
         "It is decidedly so.",
-        # ... (other responses)
+         "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
         "Very doubtful."
     ]
 
